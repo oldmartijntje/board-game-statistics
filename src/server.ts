ffbba458a40e80519794c419b28bfb6aa2ead74b
Martijn <oldmartijntje@gmail.com>
2025-04-27 16:52:39 +0200

    added back the basic pages

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import * as dotenv from "dotenv";
 import cors from "cors";
+import path from 'path';
+import fs from 'fs';
 import express from "express";
 import { connectToDatabase } from "./mainDatabase";
 import { loginRouter } from "./controllers/login.routes";
-import { Worker, MessageChannel } from 'worker_threads';
+import { static as expressStatic } from 'express';
 
 // Load environment variables from the .env file, where the MONGO_URI is configured
 // my localhost .env: "MONGO_URI=mongodb://localhost:27017/BG_STATS_WEB"
@@ -15,16 +17,50 @@ if (!MONGO_URI) {
     console.error("No MONGO_URI environment variable has been defined in config.env");
     process.exit(1);
 }
+// Function to load settings from a JSON file
+async function loadSettings(settingsPath: string) {
+    try {
+        const data = await fs.promises.readFile(settingsPath, 'utf8');
+        const settings = JSON.parse(data);
+        return settings;
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error(`Settings file not found: ${settingsPath}`);
+        } else if (error instanceof SyntaxError) {
+            console.error(`Error decoding JSON from settings file: ${settingsPath}`);
+        } else {
+            console.error(`Error reading settings file: ${error.message}`);
+        }
+        return null;
+    }
+}
 
-connectToDatabase(MONGO_URI)
-    .then(async () => {
+// Main function to execute on startup
+async function main() {
+    const settings = await loadSettings('settings.json');
+
+    if (!settings) {
+        return;
+    }
+}
+
+// Run the application on startup
+main().then(async () => {
+    connectToDatabase(MONGO_URI).then(async () => {
+        const settings = require('../settings.json');
+        const port = settings.port || 3000;
+        const staticHtmlPath = path.join(__dirname, '../docs');
         const app = express();
         app.use(cors());
         app.use("/login", loginRouter);
+        app.use(expressStatic(staticHtmlPath));
         // start the Express server
-        app.listen(5200, () => {
-            console.log(`Server running at http://localhost:5200...`);
+        app.listen(port, () => {
+            console.log(`Server running at http://localhost:${port}...`);
         });
+        app.use((req, res) => {
+            res.status(404).sendFile(path.join(staticHtmlPath, '../docs/404.html'));
+        });
+    }).catch(error => console.error(error));
 
-    })
-    .catch(error => console.error(error));
+}).catch(error => console.error(error));
