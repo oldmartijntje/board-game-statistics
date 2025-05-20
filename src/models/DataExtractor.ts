3f65f138979d01f2bd43db47033ecdc28ecee97b
OldMartijntje_Frituurpan <oldmartijntje@gmail.com>
2025-05-20 22:10:42 +0200

    relative path to tsconfig

---
+++ src/models/DataExtractor.ts
@@ -0,0 +1,3 @@
+export class DataExtractor {
+    constructor() { }
+}
--- src/threading/worker.ts
+++ src/threading/worker.ts
@@ -1,17 +1,50 @@
 import workerMessage from '../../src/dto/workerMessage/workerMessage.interface';
-import { connectToDatabase } from '../../src/mainDatabase';
+import { connectToDatabase, queueItems } from '../../src/mainDatabase';
 import { parentPort, workerData } from 'worker_threads';
 import { WorkerEnum } from './WorkerEnum';
+import { PlaySubmitter } from '../models/PlaySubmitter';
+import { QueueItemInterface } from '../../src/dto/queueItem/queueItem.interface';
+import { DataExtractor } from '../../src/models/DataExtractor';
 
 const { MONGO_URI } = process.env;
 
 if (!parentPort) {
     throw new Error('No parentPort');
 }
+
+let stepId: NodeJS.Timeout = undefined;
+
 console.log('Worker started');
+
 connectToDatabase(MONGO_URI).then(async () => {
     console.log('Worker connected to Database')
+    stepId = setInterval(() => {
+        step().catch(err => console.error("Step error:", err));
+    }, 1000 * 60)
+    step()
 });
+
+let checkinTime: Date = undefined;
+
+async function step() {
+    // clearInterval(stepId); // stop the repetition
+    checkinTime = new Date(Date.now());
+    console.log(`Worker check-in at ${checkinTime}`);
+    let optionalQueueItem: QueueItemInterface | null = null;
+    try {
+        optionalQueueItem = await queueItems.findOne();
+        if (!optionalQueueItem) {
+            return;
+        }
+    } catch (error) {
+        console.error("Error fetching queue item:", error);
+    }
+    let dataExtractor = new DataExtractor();
+    let queueItem: QueueItemInterface = optionalQueueItem;
+
+}
+
+
 
 parentPort.on('message', async (data: workerMessage) => {
     if (data.type == WorkerEnum.GetStatus) {
